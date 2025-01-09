f311ae3e6528e4dec7a4002adea706c3f639c293
AyoubBigwall <99541030+AyoubHdn@users.noreply.github.com>
2025-01-09 18:18:21 +0100

    Fix responsibility for Header and Footer

--- src/component/Footer.tsx
+++ src/component/Footer.tsx
@@ -3,15 +3,17 @@ import { PrimaryLink } from "./PrimaryLink";
 
 export function Footer() {
   return (
-    <footer className="dark:bg-gray-900">
-      <div className="container mx-auto grid h-24 grid-cols-4 items-center bg-gray-300 dark:bg-gray-900 text-center">
-        <div>
-        <PrimaryLink href="/">NameDesignAi.com</PrimaryLink><br/>
-        <Link target="_blank" href="https://hdnstudio.com/">HDN STUDIO LTD</Link>
+    <footer className="bg-gray-300 dark:bg-gray-900 pb-24 ">
+      <div className="container mx-auto flex flex-col justify-between gap-4 px-2 pt-8 text-center md:flex-row">
+        <div className="flex flex-col gap-2">
+          <PrimaryLink href="/">NameDesignAi.com</PrimaryLink><br/>
+          <Link target="_blank" href="https://hdnstudio.com/">HDN STUDIO LTD</Link>
         </div>
-        <PrimaryLink href="/privacy-policy">Privacy Policy</PrimaryLink>
-        <PrimaryLink href="/terms-of-service">Terms of Service</PrimaryLink>
-        <PrimaryLink href="/refund">Refund Policy</PrimaryLink>
+        <div className="flex justify-between gap-8 px-4">
+          <PrimaryLink href="/privacy-policy">Privacy Policy</PrimaryLink>
+          <PrimaryLink href="/terms-of-service">Terms of Service</PrimaryLink>
+          <PrimaryLink href="/refund">Refund Policy</PrimaryLink>
+        </div>
         
       </div>
     </footer>
--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -1,15 +1,40 @@
+import { useState, useEffect, useRef } from "react";
 import { signIn, signOut, useSession } from "next-auth/react";
 import { Button } from "./Button";
 import { PrimaryLink } from "./PrimaryLink";
 import { api } from "~/utils/api";
 import Image from "next/image";
+import Link from "next/link";
 
 export function Header() {
     const session = useSession();
-
     const credits = api.user.getCredits.useQuery();
-
     const isLoggedIn = !!session.data;
+
+    // State for toggling the dropdown menu
+    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+    // Reference to the dropdown for detecting outside clicks
+    const dropdownRef = useRef(null);
+
+    // Close dropdown when clicking outside
+    useEffect(() => {
+        const handleOutsideClick = (event: MouseEvent) => {
+            if (dropdownRef.current && !(dropdownRef.current as HTMLElement).contains(event.target as Node)) {
+                setIsDropdownOpen(false);
+            }
+        };
+
+        if (isDropdownOpen) {
+            document.addEventListener("mousedown", handleOutsideClick);
+        } else {
+            document.removeEventListener("mousedown", handleOutsideClick);
+        }
+
+        return () => {
+            document.removeEventListener("mousedown", handleOutsideClick);
+        };
+    }, [isDropdownOpen]);
 
     return (
         <header className="container mx-auto flex h-16 items-center justify-between px-4 dark:bg-gray-800">
@@ -26,40 +51,49 @@ export function Header() {
                     </PrimaryLink>
                 </li>
                 <li>
-                    <PrimaryLink href="/"><strong>Name Desing AI</strong></PrimaryLink>
+                    <PrimaryLink href="/" className="inline-block md:hidden xl:inline-block">
+                        <strong>Name Design AI</strong>
+                    </PrimaryLink>
                 </li>
                 <li>
-                    <PrimaryLink href="/generate">Generate</PrimaryLink>
+                    <PrimaryLink href="/generate" className="hidden md:block">
+                        Generate
+                    </PrimaryLink>
                 </li>
                 <li>
-                    <PrimaryLink href="/community">Community</PrimaryLink>
+                    <PrimaryLink href="/community" className="hidden md:block">
+                        Community
+                    </PrimaryLink>
                 </li>
                 {isLoggedIn && (
                     <li>
-                        <PrimaryLink href="/collection">Collection</PrimaryLink>
+                        <PrimaryLink href="/collection" className="hidden md:block">
+                            Collection
+                        </PrimaryLink>
                     </li>
                 )}
             </ul>
-            <ul className="flex gap-4 items-center">
+            <ul className="hidden gap-4 items-center md:flex">
                 {isLoggedIn && (
                     <>
                         <li className="flex items-center gap-2">
-    <div className="w-8 h-8 flex items-center justify-center bg-blue-500 text-white text-sm font-bold rounded-full">
-        {credits.data ?? 0}
-    </div>
-    <span className="text-sm font-medium text-gray-500">Credits</span>
-</li>
-
-
+                            <div className="w-8 h-8 flex items-center justify-center bg-blue-500 text-white text-sm font-bold rounded-full">
+                                {credits.data ?? 0}
+                            </div>
+                            <span className="text-sm font-medium text-gray-500">Credits</span>
+                        </li>
                         <li>
                             <PrimaryLink href="/buy-credits">
                                 <Button>Buy Credits</Button>
                             </PrimaryLink>
                         </li>
                         <li>
-                            <Button variant="secondary" onClick={() => {
-                                signOut().catch(console.error);
-                            }}>
+                            <Button
+                                variant="secondary"
+                                onClick={() => {
+                                    signOut().catch(console.error);
+                                }}
+                            >
                                 Logout
                             </Button>
                         </li>
@@ -67,14 +101,114 @@ export function Header() {
                 )}
                 {!isLoggedIn && (
                     <li>
-                        <Button onClick={() => {
-                            signIn().catch(console.error);
-                        }}>
+                        <Button
+                            onClick={() => {
+                                signIn().catch(console.error);
+                            }}
+                        >
                             Login
                         </Button>
                     </li>
                 )}
             </ul>
+            {/* Dropdown Toggle Button */}
+            <button
+                onClick={() => setIsDropdownOpen(!isDropdownOpen)}
+                className="relative focus:outline-none md:hidden"
+            >
+                {isDropdownOpen ? "X" : "☰"}
+            </button>
+            {/* Dropdown Menu */}
+            {isDropdownOpen && (
+                <div ref={dropdownRef} className="absolute top-14 z-10 w-full p-4 md:block md:w-auto">
+                    <ul className="rounded-lg bg-white p-4 text-slate-800 dark:bg-gray-900 dark:text-slate-200">
+                        {isLoggedIn && (
+                            <>
+                                <li>
+                                    <Link
+                                        href="/buy-credits"
+                                        className="block px-4 py-2 dark:text-white hover:bg-gray-700"
+                                        onClick={() => setIsDropdownOpen(false)}
+                                    >
+                                        Buy Credits ({credits.data ?? 0} credits left)
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link
+                                        href="/generate"
+                                        className="block px-4 py-2 dark:text-white hover:bg-gray-700"
+                                        onClick={() => setIsDropdownOpen(false)}
+                                    >
+                                        Generate
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link
+                                        href="/community"
+                                        className="block px-4 py-2 dark:text-white hover:bg-gray-700"
+                                        onClick={() => setIsDropdownOpen(false)}
+                                    >
+                                        Community
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link
+                                        href="/collection"
+                                        className="block px-4 py-2 dark:text-white hover:bg-gray-700"
+                                        onClick={() => setIsDropdownOpen(false)}
+                                    >
+                                        Collection
+                                    </Link>
+                                </li>
+                                <li>
+                                    <button
+                                        onClick={() => {
+                                            signOut().catch(console.error);
+                                            setIsDropdownOpen(false);
+                                        }}
+                                        className="block w-full text-left px-4 py-2 dark:text-white hover:bg-gray-700"
+                                    >
+                                        Sign Out
+                                    </button>
+                                </li>
+                            </>
+                        )}
+                        {!isLoggedIn && (
+                            <>
+                                <li>
+                                    <button
+                                        onClick={() => {
+                                            signIn().catch(console.error);
+                                            setIsDropdownOpen(false);
+                                        }}
+                                        className="block w-full text-left px-4 py-2 dark:text-white hover:bg-gray-700"
+                                    >
+                                        Login
+                                    </button>
+                                </li>
+                                <li>
+                                    <Link
+                                        href="/generate"
+                                        className="block px-4 py-2 dark:text-white hover:bg-gray-700"
+                                        onClick={() => setIsDropdownOpen(false)}
+                                    >
+                                        Generate
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link
+                                        href="/community"
+                                        className="block px-4 py-2 dark:text-white hover:bg-gray-700"
+                                        onClick={() => setIsDropdownOpen(false)}
+                                    >
+                                        Community
+                                    </Link>
+                                </li>
+                            </>
+                        )}
+                    </ul>
+                </div>
+            )}
         </header>
     );
 }
