36c8c5264ff7104ddb35b7dade94e5bad1fa2d39
AyoubHdn <ayoubhdn1@gmail.com>
2026-02-10 12:35:05 +0100

    debug_mode

--- src/lib/ga.ts
+++ src/lib/ga.ts
@@ -1,7 +1,12 @@
 // src/lib/ga.ts
 export function trackGA(event: string, params?: Record<string, any>) {
   if (typeof window === "undefined") return;
-  if (!("gtag" in window)) return;
+  if (!("gtag" in window)) {
+    window.dataLayer = window.dataLayer ?? [];
+    window.gtag = (...args: any[]) => {
+      window.dataLayer.push(args);
+    };
+  }
 
   window.gtag("event", event, params ?? {});
 }
